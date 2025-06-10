83cc7e083763b78ff62b95af4b4be3a51c127dde
bangd1nh <dinhbang121@gmail.com>
2025-06-10 09:10:33 +0700

    show jobs of company

--- src/hooks/company.js
+++ src/hooks/company.js
@@ -1,11 +1,17 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { fetchCompanyProfile } from "../api/company";
+import { fetchCompanyJobs, fetchCompanyProfile } from "../api/company";
 
 export const useCompany = (companyId) => {
     const CompanyProfile = useQuery({
         queryKey: ["CompanyProfile"],
         queryFn: () => fetchCompanyProfile(companyId),
     });
-    return { CompanyProfile };
+
+    const CompanyJos = useQuery({
+        queryKey: ["CompanyJobs"],
+        queryFn: () => fetchCompanyJobs(companyId),
+    });
+
+    return { CompanyProfile, CompanyJos };
 };
