0f933719e26f765828623384b764ad73087ff1e8
bangd1nh <dinhbang121@gmail.com>
2025-06-12 10:04:19 +0700

    fix synstax error ?

--- src/pages/Employer/CompanyProfile.jsx
+++ src/pages/Employer/CompanyProfile.jsx
@@ -107,6 +107,7 @@ function CompanyProfile() {
                                     </div>
                                 </div>
                             </div>
+                        </div>
                         <div className="md:col-span-3 space-y-6">
                             <div>
                                 <div className="h-4 bg-gray-300 rounded w-1/3 mb-2"></div>
